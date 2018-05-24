0d313124fc8c19ae67981a142e88e970068dc3bd
Celia Conde <celia_conde@yahoo.es>
2018-05-24 17:49:55 +0200

    tests

--- main.js
+++ main.js
@@ -1,15 +1,57 @@
 const application = function(){
+'use strict'
 
-    function getValue(){
-        var number = 3;
-        return number
-        console.log(number);
-    }
+        function start() {
+            
+        }
+
+
+        function numberNotString(number){
+            if (!typeof number == 'number'){
+                return '¡Introduce un número!';
+            }
+            else {
+                return number
+            }
+        }
+
+        function sumNumber(a, b) {
+            return a + b
+        }
+
+        function subtrackNumber(a, b) {
+            return a - b
+        }
+
+        function multiplyNumber(a, b) {
+            return a * b
+        }
+
+        function divisionNumber(a, b) {
+            return a / b
+        }
+
+
+    
+
+
+
+
+
+
+
+
+
 
     return {
-        start: getValue
+        start: numberNotString, 
+        sum: sumNumber,
+        subtrack: subtrackNumber,
+        multiply: multiplyNumber,
+        division: divisionNumber
     }
 }
+
 
 
 if (typeof(module) != 'undefined') { //Esto lo haces por buenas prácticas y seguridad
--- main.test.js
+++ main.test.js
@@ -3,11 +3,26 @@ var app;
 app = application();
 
 describe('Calculator', function(){
-    var number;
 
     it('checks if value is a number, not a string', function(){
-    expect(app.start(number)).toBe(3);
+    expect(app.start(typeof 3)).toBe('number');
     });
-});
 
+    it('sum a and b', function(){
+    expect(app.sum(2, 3)).toBe(5);
+    });
+
+    it('subtrack a and b', function(){
+    expect(app.subtrack(2, 3)).toBe(-1);
+    });
+
+    it('multiply a and b', function(){
+    expect(app.multiply(2, 3)).toBe(6);
+    });
+
+    it('division a and b', function(){
+    expect(app.division(6, 3)).toBe(2);
+    });
+
+});
 
